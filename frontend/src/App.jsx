16b942d9e3a0c46f924c8539aefce40dfa17f0e0
Shashwati12 <shashwatimeshram12@gmail.com>
2025-09-10 12:40:29 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-blue-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        React + Tailwind setup ✅
+      </h1>
+    </div>
+  );
+}
+
+export default App;
+
